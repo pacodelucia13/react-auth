2c306b654e38afc2099303a10499917027dacbe8
pacodelucia13 <guitar13>
2016-09-07 12:16:23 +0300

    adding all files

---
+++ src/components/Index.js
@@ -0,0 +1,17 @@
+// src/components/Index.js
+
+import React, { Component } from 'react';
+
+class IndexComponent extends Component {
+
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <h2>Click on a contact to view their profile</h2>
+    );
+  }
+}
+
+export default IndexComponent;
