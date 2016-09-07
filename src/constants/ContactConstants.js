494ca57413b08d4e61caa9d12a3bc4227ed8b78f
pacodelucia13 <guitar13>
2016-09-07 12:39:14 +0300

    adding more stuff, stores and actions

---
+++ src/constants/ContactConstants.js
@@ -0,0 +1,10 @@
+// src/constants/ContactConstants.js
+
+import keyMirror from 'keymirror';
+
+export default keyMirror({
+    RECEIVE_CONTACT: null,
+    RECEIVE_CONTACTS: null,
+    RECEIVE_CONTACT_ERROR: null,
+    RECEIVE_CONTACTS_ERROR: null
+});
